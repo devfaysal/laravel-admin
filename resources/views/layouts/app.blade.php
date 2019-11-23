defd481b0962067ca73550471a98197adb95ad5f
Faysal Ahamed <miufaysal@live.com>
2019-11-23 12:41:45 +0600

    Logged in users name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -105,7 +105,7 @@
                                 <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
                                     <div class="img" style="background-image: url('https://avatars3.githubusercontent.com/u/3959008?v=3&s=40')">
                                     </div>
-                                    <span class="name"> John Doe </span>
+                                    <span class="name"> {{Auth::user()->name}} </span>
                                 </a>
                                 <div class="dropdown-menu profile-dropdown-menu" aria-labelledby="dropdownMenu1">
                                     <a class="dropdown-item" href="#">
