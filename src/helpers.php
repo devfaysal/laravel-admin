3e31cd1b30d563fea8315ad87044e7f7631fce7e
Faysal Ahamed <miufaysal@live.com>
2020-02-16 16:32:08 +0600

    Multiple select

--- src/helpers.php
+++ src/helpers.php
@@ -22,11 +22,14 @@ if (! function_exists('prepare_options')) {
         if($data instanceof Illuminate\Support\Collection){
             $data = $data->toArray();
         }
+        if(!is_array($selected)){
+            $selected = [$selected];
+        }
         $options_html = '';
         
         if(array_values($data) !== $data){
             foreach($data as $key => $value){
-                if($key == $selected){
+                if(in_array($key, $selected)){
                     $options_html .= '<option selected="selected" value="'. $key .'">'. $value.'</option>';
                 }else{
                     $options_html .= '<option value="'. $key .'">'. $value.'</option>';
@@ -34,7 +37,7 @@ if (! function_exists('prepare_options')) {
             }
         }else{
             foreach($data as $key){
-                if($key == $selected){
+                if(in_array($key, $selected)){
                     $options_html .= '<option selected="selected" value="'. $key .'">'. $key.'</option>';
                 }else{
                     $options_html .= '<option value="'. $key .'">'. $key.'</option>';
