ed0ae0dfa91b74bdecacb36cc6c0cac42ace0801
Faysal Ahamed <miufaysal@live.com>
2020-10-08 11:06:56 +0600

    Update example page

--- resources/views/formFields.blade.php
+++ resources/views/formFields.blade.php
@@ -10,16 +10,16 @@
                         <div class="title-block">
                             <h4 class="title">All form fields</h4>
                         </div>
-                        <x-text-field name="text" label="Text Field" tooltip="Tooltip" placeholder="lorem"/>
-                        <x-textarea-field name="textarea" label="Textarea Field"/>
-                        <x-select-field name="select" label="Select Field"/>
+                        <x-text-field name="text" value="Some Value" label="Text Field" tooltip="Tooltip" placeholder="lorem"/>
+                        <x-textarea-field name="textarea" value="Some Text" label="Textarea Field"/>
+                        <x-select-field name="select" :data="[1,2,3,4,5]" label="Select Field"/>
                         <x-password-field name="password" label="Password Field"/>
-                        <x-number-field name="number" label="Number Field" min="0"/>
+                        <x-number-field name="number" value="5" label="Number Field" min="0"/>
                         <x-hidden-field name="hidden"/>
                         <x-file-field name="file" label="File Field"/>
-                        <x-email-field name="email" label="Email Field"/>
-                        <x-date-field name="date" label="Date Field"/>
-                        <x-checkbox-field name="checkbox" label="Checkbox Field" value=""/>
+                        <x-email-field name="email" value="email@example.com" label="Email Field"/>
+                        <x-date-field name="date" value="2020-12-12" label="Date Field"/>
+                        <x-checkbox-field name="checkbox" label="Checkbox Field" value="1"/>
 
                         <div class="form-group">
                             <input type="submit" class="btn btn-sm btn-success" value="Create">
