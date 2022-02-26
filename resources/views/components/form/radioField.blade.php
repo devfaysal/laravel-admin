76661f0d172e3e0c9b71f7eb5eb23096ff8b8f04
Faysal Ahamed <miufaysal@live.com>
2022-02-26 16:06:15 +0600

    Update radioField.blade.php

--- resources/views/components/form/radioField.blade.php
+++ resources/views/components/form/radioField.blade.php
@@ -1,8 +1,8 @@
 <div class="form-group">
-    @props(['name' => '', 'id' => $name, 'value' => '', 'label' => null, 'tooltip', 'data' => [] ])
+    @props(['name' => '', 'id' => $name, 'value' => '', 'label' => null, 'tooltip', 'data' => [], 'inline' => false ])
 
     @if($label)
-        <label for="{{ $id }}" class="control-label">{{ __($label) }}</label>
+        <label for="{{ $id }}" class="control-label {{ $inline ? 'd-block' : ''}}">{{ __($label) }}</label>
     @endif
     
     @if(isset($tooltip))
@@ -10,7 +10,7 @@
     @endif
 
     @foreach ($data as $v => $label)
-        <div class="custom-control custom-radio">
+        <div class="custom-control custom-radio {{ $inline ? 'd-inline-block pb-3' : ''}}">
             <input type="radio" id="{{ $v }}" name="{{ $name }}" {{$v == old($name, $value) ? 'checked' : ''}} value="{{ $v }}" {{ $attributes->merge(['class' => 'custom-control-input ' . ($errors->has($name) ? ' is-invalid':'')]) }}>
             <label class="custom-control-label" for="{{ $v }}">{{ $label }}</label>
         </div>
